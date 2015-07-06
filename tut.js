c097ed12f2cdb13509c1dfd7440d0ef5025d5250
Shikhar Kohli <shkohli@adobe.com>
2015-07-06 18:10:17 +0530

    Adding the gitignore file and making some JS changes

--- tut.js
+++ tut.js
@@ -27,7 +27,7 @@
              this.$el.html(this.template(this.model.toJSON()));
          }
 
-     })
+     });
 
      var todoList = new app.TodoList();
 
@@ -42,7 +42,7 @@
      view.render();
 
      app.AppView = Backbone.View.extend({
-        el: $("#todoapp");
+        el: $("#todoapp"),
         initialize: function()
         {
             this.input = this.$("#task-input");
@@ -50,14 +50,17 @@
             app.todoList.on('reset', this.resetTask, this);
             app.todoList.fetch();   
 
-        }
+        },
         addTask: function()
         {
             var newTask = new app.TodoView({model:todo});
 
             $("#todoapp").append(newTask.render().el);
+        },
+        render: function()
+        {
+            this.$el.html('Some text');
         }
      });
 
-     var app.AppView = new app.AppView;
  });
